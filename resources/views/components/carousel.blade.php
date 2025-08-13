594ab569af51fb0b6fd73dcc0819719198fd56c3
Moustafa El Dehni <moustafa.eldehni@hotmail.com>
2025-08-13 17:16:15 +0200

    Section services fini

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://cdn2.nbcuni.com/NBCUniversal/2025-03/TheOfficeHeroArt.jpg?VersionId=s4xpdQIKU5MYk8Avu6ouhaGqsG0Tx0kh" class="d-block w-100" alt="...">
+      <img src="https://cdn2.nbcuni.com/NBCUniversal/2025-03/TheOfficeHeroArt.jpg?VersionId=s4xpdQIKU5MYk8Avu6ouhaGqsG0Tx0kh" class="d-block w-100 rounded-5" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://thumb.canalplus.pro/http/unsafe/1440x810/filters:quality(80)/img-hapi.canalplus.pro:80/ServiceImage/ImageID/564673" class="d-block w-100" alt="...">
+      <img src="https://thumb.canalplus.pro/http/unsafe/1440x810/filters:quality(80)/img-hapi.canalplus.pro:80/ServiceImage/ImageID/564673" class="d-block w-100 rounded-5" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://m.media-amazon.com/images/S/pv-target-images/77ab331d48eefde4fbc6d480020b643a48b7d4016efabcf4d783369dd41325a8._SX1080_FMjpg_.jpg" class="d-block w-100" alt="...">
+      <img src="https://m.media-amazon.com/images/S/pv-target-images/77ab331d48eefde4fbc6d480020b643a48b7d4016efabcf4d783369dd41325a8._SX1080_FMjpg_.jpg" class="d-block w-100 rounded-5" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,23 +8,30 @@
     <h1 class="mt-5">Bienvenue chez Dunder Mifflin Papier & Co.</h1>
     <p>Fournisseur officiel de papier de qualité douteuse… mais avec beaucoup de cœur ! Depuis notre siège de Scranton, nous livrons nos produits aux quatre coins de la région, parfois même dans les délais. Notre équipe dévouée (et parfois un peu trop enthousiaste) est prête à vous offrir le meilleur service possible, tant que personne ne déclenche l’alarme incendie par erreur.</p>
     <section>
-        <h1 class="text-center mt-5 bg-primary text-white p-2 rounded">Nos services</h1>
-        <div class="d-flex align-items-center mt-5">
-            <div class="w-50">
-                <h2>Fourniture de papier</h2>
-                <p>De la rame classique A4 au papier parchemin « édition spéciale Michael Scott », nous avons tout ce qu’il vous faut pour remplir vos imprimantes et vos poubelles.</p>
+        <h1 class="text-center mt-5 bg-dark text-white shadow p-4 rounded">Nos services</h1>
+        <div class="d-flex flex-column gap-5">
+            <div class="d-flex align-items-center bg-light p-5 gap-5 shadow rounded-3">
+                <div class="w-50">
+                    <h2>Fourniture de papier</h2>
+                    <p>De la rame classique A4 au papier parchemin « édition spéciale Michael Scott », nous avons tout ce qu’il vous faut pour remplir vos imprimantes et vos poubelles.</p>
+                </div>
+                <img class="w-50 rounded-5" src="https://newsfeed.time.com/wp-content/uploads/sites/9/2011/11/dundermifflin.jpg?w=600" alt="">
             </div>
-            <img class="w-50" src="https://newsfeed.time.com/wp-content/uploads/sites/9/2011/11/dundermifflin.jpg?w=600" alt="">
-        </div>
-        <div class="d-flex align-items-center mt-5">
-            <div class="w-50">
-                <h2>Conseils personnalisés</h2>
-                <p>Nos vendeurs (certains plus motivés que d’autres) vous guident dans vos choix comme si vous étiez leur seul client… même si Stanley est en pause café.</p>
+            <div class="d-flex align-items-center bg-light p-5 gap-5 shadow rounded-3">
+                <div class="w-50">
+                    <h2>Conseils personnalisés</h2>
+                    <p>Nos vendeurs (certains plus motivés que d’autres) vous guident dans vos choix comme si vous étiez leur seul client… même si Stanley est en pause café.</p>
+                </div>
+                <img class="w-50 rounded-5" src="https://www.nbc.com/sites/nbcblog/files/styles/scale_862/public/2023/08/the-office-character-recap-oscar-nunez.jpg" alt="">
             </div>
-            <img class="w-50" src="https://www.nbc.com/sites/nbcblog/files/styles/scale_862/public/2023/08/the-office-character-recap-oscar-nunez.jpg" alt="">
+            <div class="d-flex align-items-center bg-light p-5 gap-5 shadow rounded-3">
+                <div class="w-50">
+                    <h2>Livraison rapide</h2>
+                    <p>Si Dwight conduit le camion, vous recevrez votre commande avant même de l’avoir passée. Si c’est Kevin… comptez plutôt la semaine prochaine.</p>
+                </div>
+                <img class="w-50 rounded-5" src="https://i.ytimg.com/vi/iD4MAy8jAWc/maxresdefault.jpg" alt="">
+            </div>
         </div>
-        <h2>Livraison rapide</h2>
-        <p>Si Dwight conduit le camion, vous recevrez votre commande avant même de l’avoir passée. Si c’est Kevin… comptez plutôt la semaine prochaine.</p>
     </section>
     <section>
         <h1>Pourquoi nous choisir ?</h1>
