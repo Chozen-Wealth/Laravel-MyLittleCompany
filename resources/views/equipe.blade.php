82c4443a85dbd072c318837d3da740d3cc69069e
Moustafa El Dehni <moustafa.eldehni@hotmail.com>
2025-08-13 18:27:42 +0200

    Test des cards produits et equipe

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -3,5 +3,12 @@
 @section("title", "Page equipe")
 
 @section("content")
-
+    <h1>Notre équipe</h1>
+    <div class="card" style="width: 18rem; overflow: hidden;">
+      <img src="https://external-preview.redd.it/the-new-spin-off-series-is-called-the-paper-v0-v1DvS2_EefI2sR2efoTwdjM2krc04CkVyIT3V8JJSUM.jpg?auto=webp&s=a636cda4a93ccf7cde5c69a9a5a910c8f334a861" class="card-img-top" style="object-fit: cover; width: 500px; position: relative; left: -70%;" alt="Image du produit">
+      <div class="card-body">
+        <h5 class="card-title">Nom & prénom</h5>
+        <p class="card-text">Post de l'employé</p>
+      </div>
+    </div>
 @endsection
--- resources/views/produits.blade.php
+++ resources/views/produits.blade.php
@@ -3,5 +3,12 @@
 @section("title", "Page produits")
 
 @section("content")
-
+    <h1>Nos produits</h1>
+    <div class="card" style="width: 18rem;">
+      <img src="https://m.media-amazon.com/images/I/616HbuvufcL.jpg" class="card-img-top" alt="Image du produit">
+      <div class="card-body">
+        <h5 class="card-title">Nom du produit</h5>
+        <p class="card-text">Description du produit</p>
+      </div>
+    </div>
 @endsection
